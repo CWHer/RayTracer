26d61e8ff2f65a8e1c61954ee91f457fb1b773bf
cwher <cwher@outlook.com>
2020-08-14 14:05:47 +0800

    Importance Sampling Materials

--- main1.cpp
+++ main1.cpp
@@ -1,20 +1,127 @@
-#include <iostream>
 #include "raytracer.h"
-#include <iomanip>
-inline double pdf(double x)
+
+#include "hittablelist.hpp"
+#include "sphere.hpp"
+#include "camera.hpp"
+#include "material.hpp"
+#include "movingsphere.hpp"
+#include "texture.hpp"
+#include "aarect.hpp"
+#include "box.hpp"
+#include "bvh.hpp"
+
+#include <ctime>
+
+Color ray_color(const Ray &r, const Color &background, const Hittable &world, int depth)
 {
-    return 3 * x * x / 8;
+    hit_record rec;
+    // If we've exceeded the ray bounce limit, no more light is gathered.
+    if (depth < 0)
+        return Color(0, 0, 0);
+    //use eps instead of 0. This gets rid of the shadow acne problem.
+    if (!world.hit(r, eps, infinity, rec))
+        return background;
+
+    Ray scattered;
+    Color attenutaion;
+    Color emitted = rec.mat_ptr->emitted(rec.u, rec.v, rec.p);
+    double pdf;
+    Color albedo;
+
+    if (!rec.mat_ptr->scatter(r, rec, albedo, scattered, pdf))
+        return emitted;
+
+    return emitted +
+           albedo * rec.mat_ptr->scattering_pdf(r, rec, scattered) *
+               ray_color(scattered, background, world, depth - 1) / pdf;
+}
+
+HittableList cornell_box()
+{
+    HittableList objects;
+
+    auto red = make_shared<Lambertian>(make_shared<SolidColor>(.65, .05, .05));
+    auto white = make_shared<Lambertian>(make_shared<SolidColor>(.73, .73, .73));
+    auto green = make_shared<Lambertian>(make_shared<SolidColor>(.12, .45, .15));
+    auto light = make_shared<DiffuseLight>(make_shared<SolidColor>(15, 15, 15));
+
+    objects.add(make_shared<FlipFace>(make_shared<YZRect>(0, 555, 0, 555, 555, green)));
+    objects.add(make_shared<YZRect>(0, 555, 0, 555, 0, red));
+    objects.add(make_shared<XZRect>(213, 343, 227, 332, 554, light));
+    objects.add(make_shared<FlipFace>(make_shared<XZRect>(0, 555, 0, 555, 0, white)));
+    objects.add(make_shared<XZRect>(0, 555, 0, 555, 555, white));
+    objects.add(make_shared<FlipFace>(make_shared<XYRect>(0, 555, 0, 555, 555, white)));
+
+    shared_ptr<Hittable> box1 = make_shared<Box>(Point3(0, 0, 0), Point3(165, 330, 165), white);
+    box1 = make_shared<RotateY>(box1, 15);
+    box1 = make_shared<Translate>(box1, Vec3(265, 0, 295));
+    objects.add(box1);
+
+    shared_ptr<Hittable> box2 = make_shared<Box>(Point3(0, 0, 0), Point3(165, 165, 165), white);
+    box2 = make_shared<RotateY>(box2, -18);
+    box2 = make_shared<Translate>(box2, Vec3(130, 0, 65));
+    objects.add(box2);
+
+    HittableList world;
+    world.add(make_shared<BVHnode>(objects, 0, 1));
+
+    return world;
 }
 
 int main()
 {
-    int N = 1;
-    auto sum = 0.0;
-    for (int i = 0; i < N; i++)
+    double t = std::clock();
+
+    // Image
+    const auto aspect_ratio = 1.0 / 1.0;
+    const int image_width = 500;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
+    const int max_depth = 50;
+
+    // World
+    auto lights = make_shared<HittableList>();
+    lights->add(make_shared<XZRect>(213, 343, 227, 332, 554, shared_ptr<Material>()));
+    lights->add(make_shared<Sphere>(Point3(190, 90, 190), 90, shared_ptr<Material>()));
+
+    HittableList world = cornell_box();
+
+    const Color background(0, 0, 0);
+
+    // Camera
+    Point3 lookfrom(278, 278, -800);
+    Point3 lookat(278, 278, 0);
+    Vec3 vup(0, 1, 0);
+    auto dist_to_focus = 10.0;
+    auto aperture = 0.0;
+    auto vfov = 40.0;
+    auto t0 = 0.0;
+    auto t1 = 1.0;
+
+    Camera cam(lookfrom, lookat, vup, vfov, aspect_ratio, aperture, dist_to_focus, t0, t1);
+
+    // Render
+    std::cout << "P3\n"
+              << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = image_height - 1; j >= 0; --j)
     {
-        auto x = pow(random_double(0, 8), 1. / 3.);
-        sum += x * x / pdf(x);
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        for (int i = 0; i < image_width; ++i)
+        {
+            Color pixel_color(0, 0, 0);
+            for (int k = 0; k < samples_per_pixel; ++k)
+            {
+                auto u = (i + random_double()) / (image_width - 1);
+                auto v = (j + random_double()) / (image_height - 1);
+                Ray r = cam.get_ray(u, v);
+
+                pixel_color += ray_color(r, background, world, max_depth);
+            }
+            write_color(std::cout, pixel_color, samples_per_pixel);
+        }
     }
-    std::cout << std::fixed << std::setprecision(12);
-    std::cout << "I = " << sum / N << '\n';
+
+    std::cerr << "\nDone.\n";
+    std::cerr << (std::clock() - t) / CLOCKS_PER_SEC;
 }
--- material.hpp
+++ material.hpp
@@ -12,8 +12,15 @@ class Material
 {
 public:
     virtual bool scatter(
-        const Ray &r_in, const hit_record &rec, Color &attenuation, Ray &scattered) const = 0;
+        const Ray &r_in, const hit_record &rec, Color &albedo, Ray &scattered, double &pdf) const
+    {
+        return 0;
+    }
 
+    virtual double scattering_pdf(const Ray &r_in, const hit_record &rec, const Ray &scattered) const
+    {
+        return 0;
+    }
     virtual Color emitted(double u, double v, const Point3 &p) const
     {
         return Color(0, 0, 0);
@@ -31,85 +38,92 @@ public:
     // scatter always and attenuate by its reflectance R
     // Note we could just as well only scatter with some probability p and have attenuation be albedo/p.
     bool scatter(
-        const Ray &r_in, const hit_record &rec, Color &attenuation, Ray &scattered) const override
+        const Ray &r_in, const hit_record &rec, Color &alb, Ray &scattered, double &pdf) const override
     {
-        Vec3 scatter_dir = rec.norm + random_unit_vector();
-        scattered = Ray(rec.p, scatter_dir, r_in.time()); //default time is 0
-        attenuation = albedo->value(rec.u, rec.v, rec.p);
+        auto direction = random_in_hemisphere(rec.norm);
+        scattered = Ray(rec.p, unit_vector(direction), r_in.time());
+        alb = albedo->value(rec.u, rec.v, rec.p);
+        pdf = 0.5 / pi;
         return 1;
     }
-};
-
-class Metal : public Material
-{
-private:
-    Color albedo;
-    double fuzz;
-
-public:
-    Metal(const Color &_a, double _f) : albedo(_a), fuzz(_f) {}
-    bool scatter(
-        const Ray &r_in, const hit_record &rec, Color &attenuation, Ray &scattered) const override
+    double scattering_pdf(
+        const Ray &r_in, const hit_record &rec, const Ray &scattered) const override
     {
-        Vec3 reflected = reflect(unit_vector(r_in.direction()), rec.norm);
-        scattered = Ray(rec.p, reflected + fuzz * random_in_unit_sphere());
-        attenuation = albedo;
-        // The catch is that for big spheres or grazing rays, we may scatter below the surface.
-        // We can just have the surface absorb those.
-        return dot(reflected, rec.norm) > 0;
+        auto cosine = dot(rec.norm, unit_vector(scattered.direction()));
+        return cosine < 0 ? 0 : cosine / pi;
     }
 };
 
-class Dielectric : public Material
-{
-private:
-    double ref_idx;
+// class Metal : public Material
+// {
+// private:
+//     Color albedo;
+//     double fuzz;
 
-    //Schlick Approximation
-    double schlick(double cosine, double ref_idx) const
-    {
-        auto r0 = (1 - ref_idx) / (1 + ref_idx);
-        r0 = r0 * r0;
-        return r0 + (1 - r0) * pow((1 - cosine), 5);
-    }
+// public:
+//     Metal(const Color &_a, double _f) : albedo(_a), fuzz(_f) {}
+//     bool scatter(
+//         const Ray &r_in, const hit_record &rec, Color &attenuation, Ray &scattered) const override
+//     {
+//         Vec3 reflected = reflect(unit_vector(r_in.direction()), rec.norm);
+//         scattered = Ray(rec.p, reflected + fuzz * random_in_unit_sphere());
+//         attenuation = albedo;
+//         // The catch is that for big spheres or grazing rays, we may scatter below the surface.
+//         // We can just have the surface absorb those.
+//         return dot(reflected, rec.norm) > 0;
+//     }
+// };
 
-public:
-    Dielectric(double _r) : ref_idx(_r) {}
+// class Dielectric : public Material
+// {
+// private:
+//     double ref_idx;
 
-    bool scatter(
-        const Ray &r_in, const hit_record &rec, Color &attenuation, Ray &scattered) const override
-    { //Attenuation is always 1 — the glass surface absorbs nothing
-        attenuation = Color(1, 1, 1);
-        double etai_over_etat;
-        if (rec.front_face)
-            etai_over_etat = 1.0 / ref_idx;
-        else
-            etai_over_etat = ref_idx;
+//     //Schlick Approximation
+//     double schlick(double cosine, double ref_idx) const
+//     {
+//         auto r0 = (1 - ref_idx) / (1 + ref_idx);
+//         r0 = r0 * r0;
+//         return r0 + (1 - r0) * pow((1 - cosine), 5);
+//     }
 
-        Vec3 unit_direction = unit_vector(r_in.direction());
-        double cos_theta = fmin(dot(-unit_direction, rec.norm), 1.0);
-        double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
-        //total internal reflection
-        if (etai_over_etat * sin_theta > 1.0)
-        {
-            // Must Reflect
-            Vec3 reflected = reflect(unit_direction, rec.norm);
-            scattered = Ray(rec.p, reflected);
-            return 1;
-        }
-        // Can Refract
-        double reflect_prob = schlick(cos_theta, etai_over_etat);
-        if (random_double() < reflect_prob)
-        {
-            Vec3 reflected = reflect(unit_direction, rec.norm);
-            scattered = Ray(rec.p, reflected);
-            return 1;
-        }
-        Vec3 refracted = refract(unit_direction, rec.norm, etai_over_etat);
-        scattered = Ray(rec.p, refracted);
-        return 1;
-    }
-};
+// public:
+//     Dielectric(double _r) : ref_idx(_r) {}
+
+//     bool scatter(
+//         const Ray &r_in, const hit_record &rec, Color &attenuation, Ray &scattered) const override
+//     { //Attenuation is always 1 — the glass surface absorbs nothing
+//         attenuation = Color(1, 1, 1);
+//         double etai_over_etat;
+//         if (rec.front_face)
+//             etai_over_etat = 1.0 / ref_idx;
+//         else
+//             etai_over_etat = ref_idx;
+
+//         Vec3 unit_direction = unit_vector(r_in.direction());
+//         double cos_theta = fmin(dot(-unit_direction, rec.norm), 1.0);
+//         double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
+//         //total internal reflection
+//         if (etai_over_etat * sin_theta > 1.0)
+//         {
+//             // Must Reflect
+//             Vec3 reflected = reflect(unit_direction, rec.norm);
+//             scattered = Ray(rec.p, reflected);
+//             return 1;
+//         }
+//         // Can Refract
+//         double reflect_prob = schlick(cos_theta, etai_over_etat);
+//         if (random_double() < reflect_prob)
+//         {
+//             Vec3 reflected = reflect(unit_direction, rec.norm);
+//             scattered = Ray(rec.p, reflected);
+//             return 1;
+//         }
+//         Vec3 refracted = refract(unit_direction, rec.norm, etai_over_etat);
+//         scattered = Ray(rec.p, refracted);
+//         return 1;
+//     }
+// };
 
 class DiffuseLight : public Material
 {
@@ -120,7 +134,7 @@ public:
     DiffuseLight(shared_ptr<Texture> _emit) : emit(_emit) {}
 
     bool scatter(
-        const Ray &r_in, const hit_record &rec, Color &attenuation, Ray &scattered) const override
+        const Ray &r_in, const hit_record &rec, Color &alb, Ray &scattered, double &pdf) const override
     {
         return 0;
     }
@@ -131,21 +145,21 @@ public:
     }
 };
 
-class Isotropic : public Material
-{
-private:
-    shared_ptr<Texture> albedo;
+// class Isotropic : public Material
+// {
+// private:
+//     shared_ptr<Texture> albedo;
 
-public:
-    Isotropic(shared_ptr<Texture> _a) : albedo(_a) {}
+// public:
+//     Isotropic(shared_ptr<Texture> _a) : albedo(_a) {}
 
-    bool scatter(
-        const Ray &r_in, const hit_record &rec, Color &attenuation, Ray &scattered) const override
-    {
-        scattered = Ray(rec.p, random_in_unit_sphere(), r_in.time());
-        attenuation = albedo->value(rec.u, rec.v, rec.p);
-        return 1;
-    }
-};
+//     bool scatter(
+//         const Ray &r_in, const hit_record &rec, Color &attenuation, Ray &scattered) const override
+//     {
+//         scattered = Ray(rec.p, random_in_unit_sphere(), r_in.time());
+//         attenuation = albedo->value(rec.u, rec.v, rec.p);
+//         return 1;
+//     }
+// };
 
 #endif
